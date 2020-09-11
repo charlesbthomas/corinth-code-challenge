5b92388c0e2ea1668e81d2a5ea1773f32648ac48
Ben Cooley <bmcooley@gmail.com>
2020-09-11 13:30:05 -0500

    raise project to main folder

--- star-wars-wiki/api/constants.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  BASE_SW_URL: "https://swapi.dev/api",
-  PEOPLE_ENDPOINT: "/people",
-};
--- star-wars-wiki/src/network/search.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-/**
- *
- * @param {string} name
- * @returns {Promise<[PeopleObjects]>} resolves with an array of SWAPI people
- */
-export const search = (name) =>
-  axios.get(`/api/people/search?name=${name}`).then(({ data }) => data.results);
